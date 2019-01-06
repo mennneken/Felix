23900033400585d033c92592a4503f28d8bb367e
Nathan Hage <nathanhage@gmail.com>
2019-01-06 12:46:51 +0100

    Added click-outside directive

--- src/main.js
+++ src/main.js
@@ -24,3 +24,17 @@ fb.auth.onAuthStateChanged(user => {
 })
 
 unsync() // Unsyncs store from router
+
+Vue.directive('click-outside', {
+  bind: function (el, binding, vnode) {
+    el.clickOutsideEvent = function (event) {
+      if (!(el == event.target || el.contains(event.target))) {
+        vnode.context[binding.expression](event);
+      }
+    };
+    document.body.addEventListener('click', el.clickOutsideEvent)
+  },
+  unbind: function (el) {
+    document.body.removeEventListener('click', el.clickOutsideEvent)
+  },
+});

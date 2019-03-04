b61d3b059c0c5e06b50cdbaed717864c5952751f
Nathan Hage <nathanhage@gmail.com>
2019-03-04 19:32:09 +0100

    update

--- precache-manifest.0b784ac21ee1235ec5296adaebdd4ed1.js
+++ precache-manifest.0b784ac21ee1235ec5296adaebdd4ed1.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = [
   {
-    "revision": "163ba1c0927136ac570b326682b825e3",
-    "url": "/Felix/icons/mstile-150x150.png"
+    "revision": "a8a1a8d1c82b3d80af58871810412a61",
+    "url": "/Felix/icons/safari-pinned-tab.svg"
   },
   {
-    "revision": "44f694e7c278d3e79744",
+    "revision": "71f1d1ac44c2fa9dfdb0",
     "url": "/Felix/css/app.css"
   },
   {
@@ -19,24 +19,24 @@ self.__precacheManifest = [
     "url": "/Felix/fonts/barlow.woff2"
   },
   {
-    "revision": "2118c97442792ef598941bac43f0cbd7",
-    "url": "/Felix/fonts/barlow.eot"
+    "revision": "0c0072fd1b43aa8493db5c0295a25317",
+    "url": "/Felix/fonts/barlow.woff"
+  },
+  {
+    "revision": "88567505d036580764d763a9ec4ce4d2",
+    "url": "/Felix/fonts/barlow_semi_condensed.eot"
   },
   {
     "revision": "7b05caf29c370d3da7e92b8cbf70ec19",
     "url": "/Felix/fonts/barlow_semi_condensed.woff2"
   },
   {
-    "revision": "0c0072fd1b43aa8493db5c0295a25317",
-    "url": "/Felix/fonts/barlow.woff"
+    "revision": "2118c97442792ef598941bac43f0cbd7",
+    "url": "/Felix/fonts/barlow.eot"
   },
   {
     "revision": "df2ef788fcc478f25879075e5dd6165d",
     "url": "/Felix/fonts/barlow_semi_condensed.woff"
-  },
-  {
-    "revision": "88567505d036580764d763a9ec4ce4d2",
-    "url": "/Felix/fonts/barlow_semi_condensed.eot"
   },
   {
     "revision": "90af7baeeb14e5434bc8ea30bc25d340",
@@ -59,11 +59,11 @@ self.__precacheManifest = [
     "url": "/Felix/robots.txt"
   },
   {
-    "revision": "a8a1a8d1c82b3d80af58871810412a61",
-    "url": "/Felix/icons/safari-pinned-tab.svg"
+    "revision": "163ba1c0927136ac570b326682b825e3",
+    "url": "/Felix/icons/mstile-150x150.png"
   },
   {
-    "revision": "44f694e7c278d3e79744",
+    "revision": "71f1d1ac44c2fa9dfdb0",
     "url": "/Felix/js/app.js"
   },
   {
@@ -107,8 +107,28 @@ self.__precacheManifest = [
     "url": "/Felix/icons/apple-touch-icon-76x76.png"
   },
   {
-    "revision": "48f0ec256333b5d6217624cc562b2c90",
-    "url": "/Felix/assets/images/illu_3.png"
+    "revision": "52f2e6fb26a7a8916ec058c69429b995",
+    "url": "/Felix/assets/images/mr-fox@1x.jpg"
+  },
+  {
+    "revision": "56b9d890267a4fa589d848d7f08120f0",
+    "url": "/Felix/assets/images/mr-fox@3x.jpg"
+  },
+  {
+    "revision": "ec399b88a65e8a1e3b6232a782d43bc7",
+    "url": "/Felix/assets/images/petit-prince@3x.jpg"
+  },
+  {
+    "revision": "cabc46ad313ad669aecc001a5e37f66f",
+    "url": "/Felix/assets/images/petit-prince@2x.jpg"
+  },
+  {
+    "revision": "1b51f007d7d305ed8787f06f4df3d614",
+    "url": "/Felix/assets/images/petit-prince@1x.jpg"
+  },
+  {
+    "revision": "13c06b2ad7b291f9dde356f5c07b2860",
+    "url": "/Felix/assets/images/illu_2@2x.png"
   },
   {
     "revision": "751994dbac0ce36f2e25e33f19b47c04",
@@ -119,52 +139,28 @@ self.__precacheManifest = [
     "url": "/Felix/assets/images/looking-fox@2x.jpg"
   },
   {
-    "revision": "3c48d3a96d3dec786addf3e7ab52e729",
-    "url": "/Felix/assets/images/looking-fox@1x.jpg"
-  },
-  {
-    "revision": "ec399b88a65e8a1e3b6232a782d43bc7",
-    "url": "/Felix/assets/images/petit-prince@3x.jpg"
-  },
-  {
-    "revision": "56b9d890267a4fa589d848d7f08120f0",
-    "url": "/Felix/assets/images/mr-fox@3x.jpg"
-  },
-  {
-    "revision": "cabc46ad313ad669aecc001a5e37f66f",
-    "url": "/Felix/assets/images/petit-prince@2x.jpg"
-  },
-  {
-    "revision": "e9456d063f743cb2ef0ee15fc74c24fb",
-    "url": "/Felix/assets/images/mr-fox@2x.jpg"
-  },
-  {
-    "revision": "1b51f007d7d305ed8787f06f4df3d614",
-    "url": "/Felix/assets/images/petit-prince@1x.jpg"
-  },
-  {
-    "revision": "52f2e6fb26a7a8916ec058c69429b995",
-    "url": "/Felix/assets/images/mr-fox@1x.jpg"
+    "revision": "b5db0c39b17683612dd25f11474533db",
+    "url": "/Felix/assets/images/logos/logo_vertical-fox@3x.png"
   },
   {
     "revision": "f9ea7c86dcd14d9010618a3a01d0a7d7",
     "url": "/Felix/assets/images/logos/logo_vertical-fox@2x.png"
   },
   {
-    "revision": "b5db0c39b17683612dd25f11474533db",
-    "url": "/Felix/assets/images/logos/logo_vertical-fox@3x.png"
+    "revision": "3c48d3a96d3dec786addf3e7ab52e729",
+    "url": "/Felix/assets/images/looking-fox@1x.jpg"
   },
   {
-    "revision": "046d7bc85a561a1f4971ca99742ad4b5",
-    "url": "/Felix/assets/icons/link-out-orange-red.svg"
+    "revision": "e9456d063f743cb2ef0ee15fc74c24fb",
+    "url": "/Felix/assets/images/mr-fox@2x.jpg"
   },
   {
-    "revision": "a5816dfc39540e148a7ea6dd10066a1d",
-    "url": "/Felix/assets/images/header_working-together@3x.jpg"
+    "revision": "248989e21f1d6de27bcd42d891097702",
+    "url": "/Felix/assets/images/icon_brand.svg"
   },
   {
-    "revision": "742bde79bffff4fd08c5f32174531ada",
-    "url": "/Felix/assets/images/illu_1.svg"
+    "revision": "8add71429330eb6f20bcf2a01dee3e34",
+    "url": "/Felix/assets/images/illu_1.png"
   },
   {
     "revision": "0979490a81ed2766e663fa0fa6e9b496",
@@ -175,32 +171,36 @@ self.__precacheManifest = [
     "url": "/Felix/assets/images/illu_2.png"
   },
   {
+    "revision": "48f0ec256333b5d6217624cc562b2c90",
+    "url": "/Felix/assets/images/illu_3.png"
+  },
+  {
     "revision": "35db0ad2c55a7540119eea22b3b51d22",
     "url": "/Felix/assets/images/illu_3.svg"
-  },
-  {
-    "revision": "bf59609d8a5587ee29fdd327aac37ac2",
-    "url": "/Felix/assets/images/logos/logo_vertical-fox.svg"
-  },
-  {
-    "revision": "d74f379938e89de1c974c44b3f536f5f",
-    "url": "/Felix/assets/images/logos/logo_horizontal-fox.svg"
-  },
-  {
-    "revision": "d64750c0519c14d64706aa919c92a4de",
-    "url": "/Felix/assets/images/illu_1@3x.png"
-  },
-  {
-    "revision": "13c06b2ad7b291f9dde356f5c07b2860",
-    "url": "/Felix/assets/images/illu_2@2x.png"
   },
   {
     "revision": "c65b6825d343875e813d92eb202008f1",
     "url": "/Felix/assets/images/logos/logo_heaj.svg"
   },
   {
-    "revision": "8add71429330eb6f20bcf2a01dee3e34",
-    "url": "/Felix/assets/images/illu_1.png"
+    "revision": "d64750c0519c14d64706aa919c92a4de",
+    "url": "/Felix/assets/images/illu_1@3x.png"
+  },
+  {
+    "revision": "d74f379938e89de1c974c44b3f536f5f",
+    "url": "/Felix/assets/images/logos/logo_horizontal-fox.svg"
+  },
+  {
+    "revision": "bf59609d8a5587ee29fdd327aac37ac2",
+    "url": "/Felix/assets/images/logos/logo_vertical-fox.svg"
+  },
+  {
+    "revision": "54ef663da1d9f88621e0b08eceb9af39",
+    "url": "/Felix/assets/images/illu_3@2x.png"
+  },
+  {
+    "revision": "742bde79bffff4fd08c5f32174531ada",
+    "url": "/Felix/assets/images/illu_1.svg"
   },
   {
     "revision": "17f70aa1fb688a56afcc646bb34f9a8e",
@@ -211,16 +211,12 @@ self.__precacheManifest = [
     "url": "/Felix/assets/images/illu_3@3x.png"
   },
   {
-    "revision": "54ef663da1d9f88621e0b08eceb9af39",
-    "url": "/Felix/assets/images/illu_3@2x.png"
-  },
-  {
     "revision": "0516375576bc5a2459a0a8599fca8512",
     "url": "/Felix/assets/images/illu_2.svg"
   },
   {
-    "revision": "248989e21f1d6de27bcd42d891097702",
-    "url": "/Felix/assets/images/icon_brand.svg"
+    "revision": "a5816dfc39540e148a7ea6dd10066a1d",
+    "url": "/Felix/assets/images/header_working-together@3x.jpg"
   },
   {
     "revision": "9619e805439049767da821524017acf5",
@@ -237,6 +233,10 @@ self.__precacheManifest = [
   {
     "revision": "9fd4a7a6c9e474a8c71b95d617190455",
     "url": "/Felix/assets/icons/link-out-orange.svg"
+  },
+  {
+    "revision": "046d7bc85a561a1f4971ca99742ad4b5",
+    "url": "/Felix/assets/icons/link-out-orange-red.svg"
   },
   {
     "revision": "fab8c5c1f8554c0b7503d692ee0faf9c",
